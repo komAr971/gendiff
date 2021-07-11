a814849e45879ab38036da368b71e96cef73a679
Konstantin Korostelev <korostel97@gmail.com>
2021-07-11 22:23:39 +0300

    add format option and arguments manual

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,9 @@ const program = new Command();
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 
 program.parse(process.argv);
