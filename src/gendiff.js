444edcae467af393a85dc1d41e72ca60c133ece0
Konstantin Korostelev <korostel97@gmail.com>
2021-07-20 19:46:19 +0300

    fix bug whit equal keys and values

--- src/gendiff.js
+++ src/gendiff.js
@@ -9,21 +9,18 @@ const genDiff = (json1, json2) => {
   const keys = _.uniq([...Object.keys(obj1), ...Object.keys(obj2)].sort());
   for (let i = 0; i < keys.length; i += 1) {
     if (obj1[keys[i]] === obj2[keys[i]]) {
-      result.push(`    ${keys[i]}: ${obj1[keys[i]]}`);
-    }
-    if (!_.has(obj1, keys[i]) && _.has(obj2, keys[i])) {
-      result.push(`  + ${keys[i]}: ${obj2[keys[i]]}`);
-    }
-    if (_.has(obj1, keys[i]) && !_.has(obj2, keys[i])) {
-      result.push(`  - ${keys[i]}: ${obj1[keys[i]]}`);
-    }
-    if (_.has(obj1, keys[i]) && _.has(obj2, keys[i])) {
-      result.push(`  - ${keys[i]}: ${obj1[keys[i]]}`);
-      result.push(`  + ${keys[i]}: ${obj2[keys[i]]}`);
+      result.push(`\t  ${keys[i]}: ${obj1[keys[i]]}`);
+    } else if (!_.has(obj1, keys[i]) && _.has(obj2, keys[i])) {
+      result.push(`\t+ ${keys[i]}: ${obj2[keys[i]]}`);
+    } else if (_.has(obj1, keys[i]) && !_.has(obj2, keys[i])) {
+      result.push(`\t- ${keys[i]}: ${obj1[keys[i]]}`);
+    } else if (_.has(obj1, keys[i]) && _.has(obj2, keys[i])) {
+      result.push(`\t- ${keys[i]}: ${obj1[keys[i]]}`);
+      result.push(`\t+ ${keys[i]}: ${obj2[keys[i]]}`);
     }
   }
 
-  return `{\n${result.join('\n')}\n}`;
+  return result.length > 0 ? `{\n${result.join('\n')}\n}` : '{}';
 };
 
 export default genDiff;
