f3ec6ef263f6ccd5fa8a40bf8ee5536941a25c41
Konstantin Korostelev <korostel97@gmail.com>
2022-10-13 15:37:26 +0400

    add optional value for formater

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,13 +8,17 @@ const program = new Command();
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2) => {
     const obj1 = parsers(filepath1);
     const obj2 = parsers(filepath2);
-    console.log(stylish(gendiff(obj1, obj2)));
+    if (program.opts().format === 'stylish') {
+      console.log(stylish(gendiff(obj1, obj2)));
+    } else {
+      console.log(gendiff(obj1, obj2));
+    }
   });
 
 program.parse(process.argv);
