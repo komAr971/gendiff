731b27a75ab7dadd72d7273d158320072f9861b1
Korostelev Konstantin <korostel97@tutamail.com>
2025-04-28 16:20:41 +0400

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,200 +8,39 @@ import gendiff from '../index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-test('gendiff(stylish) json as lib use', () => {
-  const filepath1 = path.join(__dirname, '..', '__fixtures__', 'json1.json');
-  const filepath2 = path.join(__dirname, '..', '__fixtures__', 'json2.json');
-  const filepath3 = path.join(__dirname, '..', '__fixtures__', 'emptyJSON.json');
+describe.each([
+  {file1: 'json1.json', file2: 'json2.json', fileExpected: 'expected12Stylish.txt', type: 'stylish'},
+  {file1: 'json1.json', file2: 'emptyJSON.json', fileExpected: 'expected1eStylish.txt', type: 'stylish'},
+  {file1: 'emptyJSON.json', file2: 'json1.json', fileExpected: 'expectede1Stylish.txt', type: 'stylish'},
+  {file1: 'emptyJSON.json', file2: 'emptyJSON.json', fileExpected: 'emptyJSON.json', type: 'stylish'},
+  {file1: 'yaml1.yaml', file2: 'yaml2.yml', fileExpected: 'expected12Stylish.txt', type: 'stylish'},
+  {file1: 'yaml1.yaml', file2: 'emptyYAML.yml', fileExpected: 'expected1eStylish.txt', type: 'stylish'},
+  {file1: 'emptyYAML.yml', file2: 'yaml1.yaml', fileExpected: 'expectede1Stylish.txt', type: 'stylish'},
+  {file1: 'emptyYAML.yml', file2: 'emptyYAML.yml', fileExpected: 'emptyYAML.yml', type: 'stylish'},
+  {file1: 'json1.json', file2: 'json2.json', fileExpected: 'expected12Plain.txt', type: 'plain'},
+  {file1: 'json1.json', file2: 'emptyJSON.json', fileExpected: 'expected1ePlain.txt', type: 'plain'},
+  {file1: 'emptyJSON.json', file2: 'json1.json', fileExpected: 'expectede1Plain.txt', type: 'plain'},
+  {file1: 'emptyJSON.json', file2: 'emptyJSON.json', fileExpected: 'expectedEmpty.txt', type: 'plain'},
+  {file1: 'yaml1.yaml', file2: 'yaml2.yml', fileExpected: 'expected12Plain.txt', type: 'plain'},
+  {file1: 'yaml1.yaml', file2: 'emptyYAML.yml', fileExpected: 'expected1ePlain.txt', type: 'plain'},
+  {file1: 'emptyYAML.yml', file2: 'yaml1.yaml', fileExpected: 'expectede1Plain.txt', type: 'plain'},
+  {file1: 'emptyYAML.yml', file2: 'emptyYAML.yml', fileExpected: 'expectedEmpty.txt', type: 'plain'},
+  {file1: 'json1.json', file2: 'json2.json', fileExpected: 'expected12Json.txt', type: 'json'},
+  {file1: 'json1.json', file2: 'emptyJSON.json', fileExpected: 'expected1eJson.txt', type: 'json'},
+  {file1: 'emptyJSON.json', file2: 'json1.json', fileExpected: 'expectede1Json.txt', type: 'json'},
+  {file1: 'emptyJSON.json', file2: 'emptyJSON.json', fileExpected: 'emptyJSON.json', type: 'json'},
+  {file1: 'yaml1.yaml', file2: 'yaml2.yml', fileExpected: 'expected12Json.txt', type: 'json'},
+  {file1: 'yaml1.yaml', file2: 'emptyYAML.yml', fileExpected: 'expected1eJson.txt', type: 'json'},
+  {file1: 'emptyYAML.yml', file2: 'yaml1.yaml', fileExpected: 'expectede1Json.txt', type: 'json'},
+  {file1: 'emptyYAML.yml', file2: 'emptyYAML.yml', fileExpected: 'emptyJSON.json', type: 'json'},
+])('gendiff', ({file1, file2, fileExpected, type}) => {
+  test(`${file1} ${file2} ${type} ${fileExpected}`, () => {
+    const filepath1 = path.join(__dirname, '..', '__fixtures__', file1);
+    const filepath2 = path.join(__dirname, '..', '__fixtures__', file2);
+    const filepathExpected = path.join(__dirname, '..', '__fixtures__', fileExpected);
+    const expected = fs.readFileSync(filepathExpected, 'utf8');
 
-  const filepath12Stylish = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expected12Stylish.txt',
-  );
-  const filepath1eStylish = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expected1eStylish.txt',
-  );
-  const filepathe1Stylish = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expectede1Stylish.txt',
-  );
-  const expected12Stylish = fs.readFileSync(filepath12Stylish, 'utf8');
-  const expected1eStylish = fs.readFileSync(filepath1eStylish, 'utf8');
-  const expectede1Stylish = fs.readFileSync(filepathe1Stylish, 'utf8');
-
-  expect(gendiff(filepath3, filepath3, 'stylish')).toBe('{}');
-  expect(gendiff(filepath1, filepath2, 'stylish')).toBe(expected12Stylish);
-  expect(gendiff(filepath1, filepath3, 'stylish')).toBe(expected1eStylish);
-  expect(gendiff(filepath3, filepath1, 'stylish')).toBe(expectede1Stylish);
+    expect(gendiff(filepath1, filepath2, type)).toBe(expected);
+  });
 });
 
-test('gendiff(stylish) yaml as lib use', () => {
-  const filepath1 = path.join(__dirname, '..', '__fixtures__', 'yaml1.yaml');
-  const filepath2 = path.join(__dirname, '..', '__fixtures__', 'yaml2.yml');
-  const filepath3 = path.join(__dirname, '..', '__fixtures__', 'emptyYAML.yml');
-
-  const filepath12Stylish = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expected12Stylish.txt',
-  );
-  const filepath1eStylish = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expected1eStylish.txt',
-  );
-  const filepathe1Stylish = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expectede1Stylish.txt',
-  );
-  const expected12Stylish = fs.readFileSync(filepath12Stylish, 'utf8');
-  const expected1eStylish = fs.readFileSync(filepath1eStylish, 'utf8');
-  const expectede1Stylish = fs.readFileSync(filepathe1Stylish, 'utf8');
-
-  expect(gendiff(filepath3, filepath3, 'stylish')).toBe('{}');
-  expect(gendiff(filepath1, filepath2, 'stylish')).toBe(expected12Stylish);
-  expect(gendiff(filepath1, filepath3, 'stylish')).toBe(expected1eStylish);
-  expect(gendiff(filepath3, filepath1, 'stylish')).toBe(expectede1Stylish);
-});
-
-test('gendiff(plain) json as lib use', () => {
-  const filepath1 = path.join(__dirname, '..', '__fixtures__', 'json1.json');
-  const filepath2 = path.join(__dirname, '..', '__fixtures__', 'json2.json');
-  const filepath3 = path.join(__dirname, '..', '__fixtures__', 'emptyJSON.json');
-
-  const filepath12Plain = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expected12Plain.txt',
-  );
-  const filepath1ePlain = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expected1ePlain.txt',
-  );
-  const filepathe1Plain = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expectede1Plain.txt',
-  );
-  const expected12Plain = fs.readFileSync(filepath12Plain, 'utf8');
-  const expected1ePlain = fs.readFileSync(filepath1ePlain, 'utf8');
-  const expectede1Plain = fs.readFileSync(filepathe1Plain, 'utf8');
-
-  expect(gendiff(filepath3, filepath3, 'plain')).toBe('');
-  expect(gendiff(filepath1, filepath2, 'plain')).toBe(expected12Plain);
-  expect(gendiff(filepath1, filepath3, 'plain')).toBe(expected1ePlain);
-  expect(gendiff(filepath3, filepath1, 'plain')).toBe(expectede1Plain);
-});
-
-test('gendiff(plain) yaml as lib use', () => {
-  const filepath1 = path.join(__dirname, '..', '__fixtures__', 'yaml1.yaml');
-  const filepath2 = path.join(__dirname, '..', '__fixtures__', 'yaml2.yml');
-  const filepath3 = path.join(__dirname, '..', '__fixtures__', 'emptyYAML.yml');
-
-  const filepath12Plain = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expected12Plain.txt',
-  );
-  const filepath1ePlain = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expected1ePlain.txt',
-  );
-  const filepathe1Plain = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expectede1Plain.txt',
-  );
-  const expected12Plain = fs.readFileSync(filepath12Plain, 'utf8');
-  const expected1ePlain = fs.readFileSync(filepath1ePlain, 'utf8');
-  const expectede1Plain = fs.readFileSync(filepathe1Plain, 'utf8');
-
-  expect(gendiff(filepath3, filepath3, 'plain')).toBe('');
-  expect(gendiff(filepath1, filepath2, 'plain')).toBe(expected12Plain);
-  expect(gendiff(filepath1, filepath3, 'plain')).toBe(expected1ePlain);
-  expect(gendiff(filepath3, filepath1, 'plain')).toBe(expectede1Plain);
-});
-
-test('gendiff(json) json as lib use', () => {
-  const filepath1 = path.join(__dirname, '..', '__fixtures__', 'json1.json');
-  const filepath2 = path.join(__dirname, '..', '__fixtures__', 'json2.json');
-  const filepath3 = path.join(__dirname, '..', '__fixtures__', 'emptyJSON.json');
-
-  const filepath12Json = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expected12Json.txt',
-  );
-  const filepath1eJson = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expected1eJson.txt',
-  );
-  const filepathe1Json = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expectede1Json.txt',
-  );
-  const expected12Json = fs.readFileSync(filepath12Json, 'utf8');
-  const expected1eJson = fs.readFileSync(filepath1eJson, 'utf8');
-  const expectede1Json = fs.readFileSync(filepathe1Json, 'utf8');
-
-  expect(gendiff(filepath3, filepath3, 'json')).toBe('{}');
-  expect(gendiff(filepath1, filepath2, 'json')).toBe(expected12Json);
-  expect(gendiff(filepath1, filepath3, 'json')).toBe(expected1eJson);
-  expect(gendiff(filepath3, filepath1, 'json')).toBe(expectede1Json);
-});
-
-test('gendiff(json) yaml as lib use', () => {
-  const filepath1 = path.join(__dirname, '..', '__fixtures__', 'yaml1.yaml');
-  const filepath2 = path.join(__dirname, '..', '__fixtures__', 'yaml2.yml');
-  const filepath3 = path.join(__dirname, '..', '__fixtures__', 'emptyYAML.yml');
-
-  const filepath12Json = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expected12Json.txt',
-  );
-  const filepath1eJson = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expected1eJson.txt',
-  );
-  const filepathe1Json = path.join(
-    __dirname,
-    '..',
-    '__fixtures__',
-    'expectede1Json.txt',
-  );
-  const expected12Json = fs.readFileSync(filepath12Json, 'utf8');
-  const expected1eJson = fs.readFileSync(filepath1eJson, 'utf8');
-  const expectede1Json = fs.readFileSync(filepathe1Json, 'utf8');
-
-  expect(gendiff(filepath3, filepath3, 'json')).toBe('{}');
-  expect(gendiff(filepath1, filepath2, 'json')).toBe(expected12Json);
-  expect(gendiff(filepath1, filepath3, 'json')).toBe(expected1eJson);
-  expect(gendiff(filepath3, filepath1, 'json')).toBe(expectede1Json);
-});
